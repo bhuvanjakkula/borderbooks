c5ebcc9fb559384bcdf14e4c4584758666410022
Bhuvan Jakkula <bhuvanjakkula@gmail.com>
2026-09-12 22:06:30 +0530

    Fix clerk auth.protect() bug in middleware

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -4,7 +4,7 @@ const protectedRoute = createRouteMatcher(["/app(.*)", "/api(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (request.nextUrl.pathname === "/api/webhooks/stripe") return;
-  if (protectedRoute(request)) await auth.protect();
+  if (protectedRoute(request)) await auth().protect();
 });
 
 export const config = { matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)", "/(api|trpc)(.*)"] };
